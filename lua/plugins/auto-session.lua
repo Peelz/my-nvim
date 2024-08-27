81619a23934bca0b0018cc2e9695ced9739b82de
Anucha M <anucha.mai@truedigital.com>
2024-08-27 10:21:06 +0700

    remove auto-session

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -1,20 +1,20 @@
 return {
-  {
-    "rmagatti/auto-session",
-    lazy = false,
-    dependencies = {
-      "nvim-telescope/telescope.nvim", -- Only needed if you want to use sesssion lens
-    },
-    keys = {
-      -- Will use Telescope if installed or a vim.ui.select picker otherwise
-      { "<leader>mr", "<cmd>SessionSearch<CR>", desc = "Session search" },
-      { "<leader>ms", "<cmd>SessionSave<CR>", desc = "Save session" },
-      { "<leader>ma", "<cmd>SessionToggleAutoSave<CR>", desc = "Toggle autosave" },
-    },
-    config = function()
-      require("auto-session").setup({
-        auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
-      })
-    end,
-  },
+  -- {
+  --   "rmagatti/auto-session",
+  --   lazy = false,
+  --   dependencies = {
+  --     "nvim-telescope/telescope.nvim", -- Only needed if you want to use sesssion lens
+  --   },
+  --   keys = {
+  --     -- Will use Telescope if installed or a vim.ui.select picker otherwise
+  --     { "<leader>mr", "<cmd>SessionSearch<CR>", desc = "Session search" },
+  --     { "<leader>ms", "<cmd>SessionSave<CR>", desc = "Save session" },
+  --     { "<leader>ma", "<cmd>SessionToggleAutoSave<CR>", desc = "Toggle autosave" },
+  --   },
+  --   config = function()
+  --     require("auto-session").setup({
+  --       auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+  --     })
+  --   end,
+  -- },
 }
