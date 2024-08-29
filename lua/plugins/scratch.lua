dc1e9d382767f5729d0a95492d813756220c0dbf
Anucha M <anucha.mai@truedigital.com>
2024-08-29 09:57:34 +0700

    add scratch plugin

---
+++ lua/plugins/scratch.lua
@@ -0,0 +1,9 @@
+return {
+  "LintaoAmons/scratch.nvim",
+  event = "VeryLazy",
+  keys = {
+    { "<leader>bs", "<cmd>Scratch<cr>", desc = "Scratch Buffer", mode = "n" },
+    { "<leader>bS", "<cmd>ScratchOpenFzf<cr>", desc = "Scratch Open Fzf", mode = "n" },
+    { "<leader>bn", "<cmd>ScratchWithName<cr>", desc = "Scratch With Name", mode = "n" },
+  },
+}
