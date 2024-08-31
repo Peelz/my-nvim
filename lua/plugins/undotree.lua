e27fdc3175d171c245868a173541a68f69b6e0ac
Anucha M <anucha.mai@truedigital.com>
2024-09-01 01:50:28 +0700

    update plugins

---
+++ lua/plugins/undotree.lua
@@ -0,0 +1,14 @@
+return {
+  {
+
+    "mbbill/undotree",
+    keys = {
+      {
+        "<leader>U",
+        "<cmd>UndotreeToggle<cr>",
+        desc = "Undotree",
+        "n",
+      },
+    },
+  },
+}
