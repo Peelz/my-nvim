857f858b5a37d1abaefb88610d9bc7afeacfb73c
Anucha M <anucha.mai@truedigital.com>
2024-09-01 02:43:00 +0700

    add zenmode

---
+++ lua/plugins/zenmode.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "folke/zen-mode.nvim",
+    opt = {},
+    keys = {
+      {
+        "<leader>z",
+        "<cmd>ZenMode<cr>",
+        desc = "Zen Mode Toggle",
+        "n",
+      },
+    },
+  },
+}
