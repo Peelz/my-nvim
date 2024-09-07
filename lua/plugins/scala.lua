9895261ffd5e1212090215d700fddd8d92635138
Anucha M <anucha.mai@truedigital.com>
2024-09-07 10:54:25 +0700

    update config

--- lua/plugins/scala.lua
+++ lua/plugins/scala.lua
@@ -1,76 +1,59 @@
 return {
   -- add treesitter support for scala
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, { "scala", "java" })
-    end,
-  },
-  {
-    "mfussenegger/nvim-dap",
-    config = function() end,
-  },
-  {
-    "scalameta/nvim-metals",
-    ft = { "scala", "sbt" },
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "mfussenegger/nvim-dap",
-    },
-    -- stylua: ignore
-    keys = {
-      { "<leader>cW", function () require('metals').hover_worksheet() end, desc = "Metals Worksheet" },
-      { "<leader>cM", function () require('telescope').extensions.metals.commands() end, desc = "Telescope Metals Commands" },
-    },
-    config = function()
-      -- Autocmd that will actually be in charging of starting the whole thing
-      local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
-
-      local metals_config = require("metals").bare_config()
-
-      -- Example of settings
-      metals_config.settings = {
-        showImplicitArguments = true,
-        excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
-      }
-
-      -- *READ THIS*
-      -- I *highly* recommend setting statusBarProvider to true, however if you do,
-      -- you *have* to have a setting to display this in your statusline or else
-      -- you'll not see any messages from metals. There is more info in the help
-      -- docs about this
-      -- metals_config.init_options.statusBarProvider = "on"
-
-      -- Example if you are using cmp how to make sure the correct capabilities for snippets are set
-      metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-      metals_config.on_attach = function(_client, _bufnr)
-        require("metals").setup_dap()
-      end
-
-      vim.api.nvim_create_autocmd("FileType", {
-        -- NOTE: You may or may not want java included here. You will need it if you
-        -- want basic Java support but it may also conflict if you are using
-        -- something like nvim-jdtls which also works on a java filetype autocmd.
-        pattern = { "scala", "sbt", "java" },
-        callback = function()
-          require("metals").initialize_or_attach(metals_config)
-        end,
-        group = nvim_metals_group,
-      })
-    end,
-  },
-
+  -- {
+  --   "nvim-treesitter/nvim-treesitter",
+  --   opts = function(_, opts)
+  --     vim.list_extend(opts.ensure_installed, { "scala", "java" })
+  --   end,
+  -- },
+  -- {
+  --   "j-hui/fidget.nvim",
+  --   tag = "legacy",
+  --   config = true,
+  -- },
+  -- {
+  --   "scalameta/nvim-metals",
+  --   ft = { "scala", "sbt" },
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --     "mfussenegger/nvim-dap",
+  --   },
+  --   -- stylua: ignore
+  --   keys = {
+  --     { "<leader>cW", function () require('metals').hover_worksheet() end, desc = "Metals Worksheet" },
+  --     { "<leader>cM", function () require('telescope').extensions.metals.commands() end, desc = "Telescope Metals Commands" },
+  --   },
+  --   opts = function()
+  --     local metals_config = require("metals").bare_config()
+  --     metals_config.on_attach = function(client, bufnr)
+  --       -- your on_attach function
+  --     end
+  --
+  --     return metals_config
+  --   end,
+  --   config = function()
+  --     local metals_config = require("metals").bare_config()
+  --     metals_config.settings = {
+  --       showInferredType = true,
+  --       excludedPackages = {
+  --         "akka.actor.typed.javadsl",
+  --         "com.github.swagger.akka.javadsl",
+  --       },
+  --     }
+  --     require("metals").initialize_or_attach(metals_config)
+  --   end,
+  -- },
+  --
   -- Correctly setup lspconfig for Scala 🚀
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "scalameta/nvim-metals",
-    },
-    opts = {
-      setup = {
-        metals = {},
-      },
-    },
-  },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   dependencies = {
+  --     "scalameta/nvim-metals",
+  --   },
+  --   opts = {
+  --     setup = {
+  --       metals = {},
+  --     },
+  --   },
+  -- },
 }
