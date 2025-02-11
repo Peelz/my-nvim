0753d95bdbf8ce23ae57491442263210c71d30ab
Anucha M <anucha.mai@truedigital.com>
2025-02-11 19:34:01 +0700

    update

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -43,16 +43,16 @@ local function copypath(state)
   end)
 end
 
--- return {
---   "nvim-neo-tree/neo-tree.nvim",
---   opts = {
---     window = {
---       mappings = {
---         ["L"] = {
---           open_all_subnodes,
---           desc = "Open nodes",
---         },
---       },
---     },
---   },
--- }
+return {
+  -- "nvim-neo-tree/neo-tree.nvim",
+  -- opts = {
+  --   window = {
+  --     mappings = {
+  --       ["L"] = {
+  --         open_all_subnodes,
+  --         desc = "Open nodes",
+  --       },
+  --     },
+  --   },
+  -- },
+}
