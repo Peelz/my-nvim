e9e1c6b343f5d61a8055d6df15c97dedc4c4f18d
Anucha M <anucha.mai@truedigital.com>
2025-02-17 23:37:23 +0700

    update cop clipboard support osc52

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -20,3 +20,23 @@ vim.filetype.add {
   },
 }
 vim.g.autoformat = false
+-- to set clipboard copy
+vim.o.clipboard = "unnamedplus"
+local function paste()
+  return {
+    vim.fn.split(vim.fn.getreg(""), "\n"),
+    vim.fn.getregtype(""),
+  }
+end
+
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = paste,
+    ["*"] = paste,
+  },
+}
