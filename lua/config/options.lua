673dcc3bf2cf184d92040f8a746df56ca08bd15a
Anucha Mai <anucha.mai@truedigital.com>
2025-02-23 20:49:59 +0700

    update option

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,9 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.nu = true
-vim.opt.relativenumber = true
--- vim.o.statuscolumn = "%s %l %r "-- LSP Server to use for Python.
+vim.o.nu = true
+vim.o.relativenumber = true
+vim.o.number = true
+vim.o.statuscolumn = "%s %l %r"
 
 -- Set to "basedpyright" to use basedpyright instead of pyright.
 vim.g.lazyvim_python_lsp = "basedpyright"
@@ -12,13 +13,13 @@ vim.g.lazyvim_python_lsp = "basedpyright"
 vim.g.lazyvim_python_ruff = "ruff"
 vim.o.termguicolors = true
 
-vim.filetype.add {
+vim.filetype.add({
   extension = {
-    jinja = 'jinja',
-    jinja2 = 'jinja',
-    j2 = 'jinja',
+    jinja = "jinja",
+    jinja2 = "jinja",
+    j2 = "jinja",
   },
-}
+})
 vim.g.autoformat = false
 -- to set clipboard copy
 vim.o.clipboard = "unnamedplus"
