2c2f9243b99691ea7161aa39e2b3f0922d31d658
Anucha Mai <anucha.mai@truedigital.com>
2025-04-10 23:21:34 +0700

    update options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,10 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.o.nu = true
+-- vim.o.nu = true
 vim.o.relativenumber = true
 vim.o.number = true
-vim.o.statuscolumn = "%s %l %r"
+vim.o.statuscolumn = "%s %l %r "
 
 -- Set to "basedpyright" to use basedpyright instead of pyright.
 vim.g.lazyvim_python_lsp = "basedpyright"
