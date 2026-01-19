63130c090954a347455655e2bfc140645bac2257
Anucha Mai <anucha.mai@truedigital.com>
2026-01-19 10:46:07 +0700

    add language support and LSP improvements

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -8,3 +8,35 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     vim.b.autoformat = false
   end,
 })
+
+-- TypeSpec filetype detection
+vim.filetype.add({
+  extension = {
+    tsp = "typespec",
+  },
+  filename = {
+    ["tspconfig.yaml"] = "yaml",
+  },
+})
+
+-- TypeSpec comment strings
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "typespec" },
+  callback = function()
+    vim.bo.commentstring = "// %s"
+  end,
+})
+
+-- Rotate LSP log if it exceeds 10MB (deferred to avoid TUI race)
+vim.api.nvim_create_autocmd("VimEnter", {
+  once = true,
+  callback = function()
+    vim.defer_fn(function()
+      local log_path = vim.fn.stdpath("state") .. "/lsp.log"
+      local stat = (vim.uv or vim.loop).fs_stat(log_path)
+      if stat and stat.size > 10 * 1024 * 1024 then
+        os.rename(log_path, log_path .. ".old")
+      end
+    end, 100)
+  end,
+})
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -13,4 +13,8 @@ return {
       },
     },
   },
+  config = function()
+    -- Limit LSP log size by setting log level to WARN
+    vim.lsp.set_log_level("WARN")
+  end,
 }
